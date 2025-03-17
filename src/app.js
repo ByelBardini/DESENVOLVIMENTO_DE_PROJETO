dd96637988f46307058742e6c486cb6916af1a1d
Gabryel Bardini <128429834+ByelBardini@users.noreply.github.com>
2025-03-17 14:50:47 -0300

    testes do app para começo da aplicação

--- src/app.js
+++ src/app.js
@@ -1,9 +1,18 @@
 import express from 'express'
 
 const app = express()
+app.use(express.json())
+
+const sintomas = [];
+const pdf = [];
 
 app.post('/sintomas', (req, res) =>{
-  res.send('Ok, deu bom')
+  console.log(req.body)
+
+  sintomas.push(req.sintomas)
+  pdf.push(req.dores)
+
+  res.status(201).json(req.body)
 })
 
 app.listen(5500)
