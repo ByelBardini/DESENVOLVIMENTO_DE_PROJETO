b48b739ece7b11a8b16288d1cb8cb49f0c251309
VictOliRodrigues <oliveirarodriguesvictor0@gmail.com>
2025-04-15 13:59:08 -0300

    corrigndo envio para o index3

--- public/js/script2.js
+++ public/js/script2.js
@@ -2,61 +2,45 @@ window.addEventListener("DOMContentLoaded", () => {
   const container = document.getElementById("sugestoes-diagnostico");
   const data = JSON.parse(localStorage.getItem("diagnostico"));
 
-  if (!data) return; // Se não houver dados, não faz nada
+  if (!data) return;
 
   const lista = Array.isArray(data) ? data : [data];
 
   lista.forEach((item) => {
+    console.log("Diagnóstico: " + item);
     const div = document.createElement("div");
     div.classList.add("sugestao");
     div.innerHTML = `<strong>${item.diagnostico}</strong><br><small>${item.descricao}</small>`;
 
     div.addEventListener("click", () => {
       document.getElementById("diagnostico").value = item.diagnostico;
-      container.innerHTML = ""; // Limpa o conteúdo após selecionar
+      container.innerHTML = ""; // limpa sugestões
     });
+
     container.appendChild(div);
   });
 });
 
-window.envia = (event) => {
-  event.preventDefault(); // Previne o recarregamento da página
-
+window.envia = () => {
   const diagnostico = document.getElementById("diagnostico").value;
 
   const options = {
     method: "POST",
     headers: {
-      "Content-Type": "application/json", // Adicionando o cabeçalho correto
+      "Content-Type": "application/json",
     },
     body: JSON.stringify({ diagnostico }),
   };
-  //Envia pro Back
+
   fetch("http://localhost:3030/diagnostico", options)
     .then((response) => response.json())
     .then((data) => {
-    
-      const container = document.getElementById("sugestoes-diagnostico");
-      container.innerHTML = ""; // Limpa o conteúdo anterior
-
-      const lista = Array.isArray(data) ? data : [data];
-
-      lista.forEach((item) => {
-        const div = document.createElement("div");
-        div.classList.add = "sugestao";
-
-        //Ao clicar, adiciona o texto no campo de texto
-        div.innerHTML = `<strong>${item.diagnostico}</strong><br><small>${item.descrição}</small>`;
-
-        div.addEventListener("click", () => {
-          document.getElementById("diagnostico").value = item.diagnostico;
-          container.innerHTML = ""; // Limpa o conteúdo após selecionar
-
-        });
-        container.appendChild(div);
-      });
+      // Armazena medicações recebidas
+      localStorage.setItem("medicacoes", JSON.stringify(data));
+      // Vai para index3
+      window.location.href = 'C:/Users/Victor - TI/Documents/Victor/projetos-faculdade/DESELVOLVIMENTO_DE_PROJETO/public/html/index3.html'
     })
     .catch((error) => {
-      console.error("Error:", error);
+      console.error("Erro ao enviar diagnóstico:", error);
     });
 };
